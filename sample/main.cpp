372c686d1a78154f220d96abe3cac49d05dbb441
Radzi <imradzi@gmail.com>
2026-01-09 01:48:14 +0800

    fixed sample code - no need checkRestructure - it's auto now

--- sample/main.cpp
+++ sample/main.cpp
@@ -4,6 +4,5 @@ int main() {
     MemberDb db;
     DB::Logger::initialize("MemberService", "debug");
     db.Open(true); // open DB, create if not exist
-    //db.CheckSchemaAndRestructure(); // restructure table is schema changed. 
     return 0;
 }
